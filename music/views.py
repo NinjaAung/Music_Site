f4e91de9dacca385f58ed2089c86ec6a19a564f8
ninjaaung <ninja.aung02@gmail.com>
2020-05-05 02:19:43 -0700

    Added Humanized

--- music/views.py
+++ music/views.py
@@ -18,7 +18,6 @@ def musician_info(request, musician_id):
     context = {
         'musician': Musician.objects.get(id=musician_id),
         'albums': Album.objects.filter(artist=musician_id)
-
     }
     return render(request, 'musician_info.html', context)
 
@@ -27,6 +26,12 @@ def album_info(request, album_id):
     context = {
         'album': Album.objects.get(id=album_id),
         'songs': Song.objects.filter(album=album_id)
-
     }
     return render(request, 'album_info.html', context)
+
+def song_info(request, song_id):
+    context = {
+        'song': Song.objects.get(id=song_id)
+    }
+    return render(request, 'song_info.html', context)
+
--- music_site/settings.py
+++ music_site/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'music.apps.MusicConfig',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
