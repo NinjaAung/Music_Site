dbdb1db6126c0876e19391db25918d5890702183
ninjaaung <ninja.aung02@gmail.com>
2020-05-05 01:26:56 -0700

    added url path to site

--- music/urls.py
+++ music/urls.py
@@ -1,7 +1,10 @@
+# music/urls.py
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('musicians/', views.musicians, name='musicians'),
+    path('musician/<int:musician_id>/', views.musician_info, name='musician_info')
 ]
--- music/views.py
+++ music/views.py
@@ -1,3 +1,4 @@
+# music/views.py
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Musician
@@ -5,3 +6,16 @@ from .models import Musician
 
 def home(request):
     return HttpResponse("Music_Site")
+
+def musicians(request):
+    context = {
+        'musician': Musician.objects.all()
+    }
+    return render(request, 'musician.html', context)
+
+
+def musician_info(request, musician_id):
+    context = {
+        'musician': Musician.objects.get(id=musician_id)
+    }
+    return render(request, 'musician_info.html', context)
